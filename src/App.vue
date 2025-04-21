7d9732c6582e0e74aaba2f7c96ebe530d076dae5
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-04-22 01:50:15 +0800

    feat: 实现基础布局

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,15 @@
 </script>
 
 <template>
-  <div class="mx-auto mt-20 w-19 h-19 bg-blue-400 text-white">
-    Hello Tailwind!
+  <div class="app flex flex-col h-screen">
+    <router-view/>
+
+    <van-tabbar class="shrink-0" placeholder>
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">搜索</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">联系</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">设置</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
+  <div class="home-view flex flex-col overflow-y-hidden">
+    <van-nav-bar class="shrink-0" fixed placeholder title="标题" />
 
+    <div class="home-view__main overflow-y-auto">
+      <van-cell-group v-for="v in 100" :title="`分组${v}`">
+        <van-cell title="单元格" value="内容" />
+      </van-cell-group>
+    </div>
+  </div>
 </template>
 
 <style scoped>
